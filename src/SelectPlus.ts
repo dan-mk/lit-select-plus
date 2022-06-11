bb4e56d14e1b325963c9a204eb4faca3b5b36fab
Daniel MK <prog43@ixcsoft.com.br>
2022-06-11 09:48:39 -0300

    Most of what I wanted to do. Next: selection through keyboard?

--- src/SelectPlus.ts
+++ src/SelectPlus.ts
@@ -9,111 +9,214 @@ interface SelectPlusOption {
 
 export class SelectPlus extends LitElement {
   static styles = css`
-    :host {
-      display: inline-block;
+    * {
+      box-sizing: border-box;
     }
 
-    .option, .value-container {
+    :host {
+      display: block;
+    }
+
+    :host,
+    .input {
+      font-family: sans-serif;
+      font-size: 15px;
+    }
+
+    .input,
+    .value-container {
+      background: white;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      padding: 8px 10px;
+      width: 100%;
+    }
+
+    .main-options-container {
+      position: relative;
+    }
+
+    .options-container {
+      background: white;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.15);
+      position: absolute;
+      top: 40px;
+      width: 100%;
+    }
+
+    .option,
+    .value-container {
       display: flex;
     }
 
-    .option-value, .value, .magnifying-glass {
+    .option {
+      padding: 10px 8px;
+      cursor: pointer;
+    }
+
+    .option:hover {
+      background: #f3f3f3;
+    }
+
+    .option-value,
+    .value,
+    .magnifying-glass {
       flex-shrink: 0;
       width: 50px;
     }
 
-    .option-label, .label, .placeholder {
+    .option-label,
+    .label,
+    .placeholder {
       flex-grow: 1;
     }
+
+    .placeholder-text {
+      color: #777;
+      font-style: italic;
+    }
+
+    .label,
+    .option-label {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    :host([hide-value]) .value,
+    :host([hide-value]) .option-value {
+      display: none;
+    }
   `;
-  
+
   @property({ type: String }) placeholder = 'Select...';
 
   @property({ type: String }) value = '';
-  
-  @state() private _searchState : Boolean = false;
 
-  @state() private _options : SelectPlusOption[] = [];
+  @property({ type: Boolean, attribute: 'hide-value' }) hideValue = false;
 
-  @state() private _query : String = '';
+  @property({ type: String, attribute: 'value-pattern' }) valuePattern =
+    '\\d{1,5}';
 
-  @state() private _label : String | null = null;
+  @state() private _searchState: Boolean = false;
+
+  @state() private _options: SelectPlusOption[] = [];
+
+  @state() private _query: String = '';
 
   firstUpdated() {
-    const options = this.querySelectorAll('select-plus-option');
-    this._options = Array.from(options).map(option => ({
-        value: option.getAttribute('value'),
-        label: option.textContent
-    })) as SelectPlusOption[];
-
     this.addEventListener('click', () => {
       if (this._searchState === false) {
         this._onClickValueContainer();
       }
     });
 
-    this.addEventListener('keydown', (e) => {
+    this.addEventListener('keydown', e => {
       if (this._searchState === false) {
         this._onKeyDownValueContainer(e);
       }
     });
+
+    this._updateOptionsList();
+
+    const observer = new MutationObserver(() => {
+      this._updateOptionsList();
+    });
+
+    observer.observe(this as Node, {
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true,
+    });
   }
 
   render() {
+    const selectedOption = this._options.find(
+      option => option.value === this.value
+    );
+    const label = selectedOption ? selectedOption.label : null;
+
     return html`
-        <div style=${styleMap({ display: (this._searchState ? 'block' : 'none')})}>
-          <input
-            type="text"
-            .value=${this._query as string}
-            @input="${this._onChangeQueryInput}"
-            @blur="${this._onBlurQueryInput}" />
-          <div>
-            ${
-              this._options.filter((option) => this._doesOptionMatchQuery(option)).map((option) => html`
-                <div class="option" @mousedown="${() => this._onMouseDownOption(option)}">
-                  <div class="option-value">
-                    ${option.value}
-                  </div>
-                  <div class="option-label">
-                    ${option.label}
-                  </div>
-                </div>
-              `)
-            }
-          </div>
-        </div>
-        <div
-          style=${styleMap({ display: (this._searchState ? 'none' : 'flex')})}
-          class="value-container">
-          ${
-            this.value !== '' ?
-              html`
-                <div class="value">
-                  ${this.value}
-                </div>
-                <div class="label">
-                  ${this._label ? this._label : 'Loading...'}
-                </div>
-              ` : html`
-                <div class="magnifying-glass">
-                  Q
-                </div>
-                <div class="placeholder">
-                  ${this.placeholder}
+      <div
+        class="main-options-container"
+        style=${styleMap({ display: this._searchState ? 'block' : 'none' })}
+      >
+        <input
+          class="input"
+          type="text"
+          .value=${this._query as string}
+          @input="${this._onChangeQueryInput}"
+          @keydown="${this._onKeyDownQueryInput}"
+          @blur="${this._onBlurQueryInput}"
+        />
+        <div class="options-container">
+          ${this._options
+            .filter(option => this._doesOptionMatchQuery(option))
+            .map(
+              option => html`
+                <div
+                  class="option"
+                  @mousedown="${() => this._onMouseDownOption(option)}"
+                >
+                  <div class="option-value">${option.value}</div>
+                  <div class="option-label">${option.label}</div>
                 </div>
               `
-          }
+            )}
         </div>
-      `;
+      </div>
+      <div
+        style=${styleMap({ display: this._searchState ? 'none' : 'flex' })}
+        class="value-container"
+      >
+        ${this.value !== ''
+          ? html`
+              <div class="value">${this.value}</div>
+              <div class="label">
+                ${label ||
+                html`<span class="placeholder-text">Loading...</span>`}
+              </div>
+            `
+          : html`
+              <div class="magnifying-glass">Q</div>
+              <div class="placeholder">
+                <span class="placeholder-text">${this.placeholder}</span>
+              </div>
+            `}
+      </div>
+    `;
   }
 
-  private _onChangeQueryInput(e : Event) {
+  private _updateOptionsList() {
+    const options = this.querySelectorAll('select-plus-option');
+    this._options = Array.from(options).map(option => ({
+      value: option.getAttribute('value'),
+      label: option.textContent,
+    })) as SelectPlusOption[];
+  }
+
+  private _onChangeQueryInput(e: KeyboardEvent) {
     const queryInput = e.target! as HTMLInputElement;
     this._query = queryInput.value;
   }
 
+  private _onKeyDownQueryInput(e: KeyboardEvent) {
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      this.focus();
+    }
+  }
+
   private _onBlurQueryInput() {
     this._searchState = false;
+
+    const pattern = new RegExp(this.valuePattern);
+    if (pattern.test(this._query as string)) {
+      this.value = this._query as string;
+    }
   }
 
   private _doesOptionMatchQuery(option: SelectPlusOption) {
@@ -123,9 +226,8 @@ export class SelectPlus extends LitElement {
     );
   }
 
-  private _onMouseDownOption(option : SelectPlusOption) {
+  private _onMouseDownOption(option: SelectPlusOption) {
     this.value = option.value;
-    this._updateLabel();
     this.updateComplete.then(() => {
       this.focus();
     });
@@ -136,18 +238,17 @@ export class SelectPlus extends LitElement {
   }
 
   private _onKeyDownValueContainer(e: KeyboardEvent) {
-    if (e.code === "Space") {
+    if (e.code === 'Space') {
       this._openOptionsContainer();
       e.preventDefault();
-    } else if (e.key === "Delete" || e.key === "Backspace") {
+    } else if (e.key === 'Delete' || e.key === 'Backspace') {
       this.value = '';
-      this._updateLabel();
     }
   }
 
   private _openOptionsContainer() {
     this._searchState = true;
-    if (this.value !== '') {
+    if (this.value !== '' && this.hideValue === false) {
       this._query = this.value;
     } else {
       this._query = '';
@@ -155,11 +256,7 @@ export class SelectPlus extends LitElement {
 
     this.updateComplete.then(() => {
       this.shadowRoot?.querySelector('input')?.focus();
+      this.shadowRoot?.querySelector('input')?.select();
     });
-  }
-
-  private _updateLabel() {
-    const selectedOption = this._options.find((option) => option.value === this.value);
-    this._label = selectedOption ? selectedOption.label : null;
   }
 }
